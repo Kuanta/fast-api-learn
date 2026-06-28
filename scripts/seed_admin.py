e5a80b20f1e1d4062b3a52561d1a34c723eb0d4a
Kuanta <1369393+Kuanta@users.noreply.github.com>
2026-06-28 15:15:14 +0300

    feat(seed): implement admin seed

---
+++ scripts/seed_admin.py
@@ -0,0 +1,39 @@
+from core.database import session_local
+from auth.models import AccountModel
+from auth.services import get_account_by_username
+from auth.utils import hash_password
+from core.config import settings
+import argparse
+
+ADMIN_USERNAME = "kuanta"
+ADMIN_EMAIL = "dorukhan.erdem@gmail.com"
+ADMIN_PASSWORD = "change_me"
+
+def seed_admin(admin_username:str, admin_raw_password:str, admin_email:str | None):
+    db = session_local()
+    try:
+        admin = get_account_by_username(admin_username, db)
+        if admin is not None:
+            print(f"Admin with {admin_username} already exists")
+            return
+        admin = AccountModel(
+            username = admin_username,
+            email = admin_email,
+            hashed_password = hash_password(admin_raw_password),
+            account_level = settings.ADMIN_ROLE_LEVEL
+        )
+        db.add(admin)
+        db.commit()
+        print(f"Admin {admin_username} created.")
+
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--username", type=str, default=ADMIN_USERNAME)
+    parser.add_argument("--password", type=str, default=ADMIN_PASSWORD)
+    parser.add_argument("--email", type=str, default=ADMIN_EMAIL)
+    args = parser.parse_args()
+
+    seed_admin(args.username, args.password, args.email)
